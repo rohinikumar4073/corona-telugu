5a16542f9998adbf33f794306f040cf295eb46a2
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-03-25 23:43:36 +0530

    Update languages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App () {
+import { FormattedMessage } from 'react-intl';
+function App (props) {
   const [allData, setAllData] = useState(null);
   const [page] = useState(1);
   const [indiaCases, setIndiaCases] = useState(null);
@@ -25,7 +24,19 @@ function App () {
   }, [page]);
   return (
     <div className="App container ">
-      <h3> Corona Cases, Recoveries and Deaths</h3>
+      <h3> <FormattedMessage id="title" /></h3>
+      <div className="form-group row">
+        <label for="changeLange" className="col-sm-4 "><FormattedMessage id="changeLanguage" /></label>
+        <div className="col-sm-8">
+
+          <select className="form-control " id="changeLange" onChange={(e) => { props.changeLanguage(e.target.value) }}
+          >
+            <option value="te" selected>తెలుగు</option>
+            <option value="en">English</option>
+
+          </select>
+        </div>
+      </div>
       <div className='row'>
         {allData ? getAllData(allData) : 'Loading...'}
       </div>
@@ -41,10 +52,10 @@ function getCases (cases, indiaCases) {
     <thead>
 
       <tr>
-        <th> Country</th>
-        <th> Cases</th>
-        <th> Recoveries</th>
-        <th> Deaths</th>
+        <th> <FormattedMessage id="country" /></th>
+        <th> <FormattedMessage id="cases" /></th>
+        <th> <FormattedMessage id="recovered" /></th>
+        <th> <FormattedMessage id="deaths" /></th>
       </tr>
     </thead>
     <tbody>
@@ -68,22 +79,22 @@ function getAllData (allData) {
 
     <tbody>
       <tr>
-        <td>Cases</td>
+        <td><FormattedMessage id="cases" /></td>
         <td>{allData.cases}</td>
 
       </tr>
       <tr>
-        <td>Recovered</td>
+        <td><FormattedMessage id="recovered" /></td>
         <td>{allData.recovered}</td>
 
       </tr>
       <tr>
-        <td>Deaths</td>
+        <td><FormattedMessage id="deaths" /></td>
         <td>{allData.deaths}</td>
 
       </tr>
       <tr>
-        <td>Updated time</td>
+        <td><FormattedMessage id="updated" /></td>
         <td>{new Date(allData.updated).toLocaleString()}</td>
 
       </tr>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider } from 'react-intl';
+import teluguMessages from './translations/te.json';
+import englishMessages from './translations/en.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let locale = 'te';
+let messages = teluguMessages;
+
+function changeLanguage (language) {
+  switch ('te') {
+    case language:
+      locale = 'te';
+      messages = teluguMessages;
+      break;
+
+    default:
+      locale = 'en';
+      messages = englishMessages;
+      break;
+  }
+  render();
+}
+function render () {
+  ReactDOM.render(
+    <React.StrictMode>
+      <IntlProvider locale={locale} messages={messages}>    <App changeLanguage={changeLanguage} />
+      </IntlProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
