fed370bb83fb348a5da93f3f2d395f9f753586a5
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-03-28 16:55:21 +0530

    Updating Dark theme filtering and new cases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css';
+
 import { FormattedMessage } from 'react-intl';
+import CaseDetails from './CaseDetails';
+import Loader from './Loader';
+
+
 function App (props) {
+
   const [allData, setAllData] = useState(null);
   const [page] = useState(1);
-  const [indiaCases, setIndiaCases] = useState(null);
-
   const [cases, setCases] = useState(null);
+
 
   useEffect(() => {
 
@@ -15,72 +21,32 @@ function App (props) {
       setAllData(data.reports[0]);
       setCases(data.reports[0].table[0]);
     })
-    fetch('https://covid19-server.chrismichael.now.sh/api/v1/ReportsByCountries/India').then((res) => res.json()).then(data => {
-      setIndiaCases(data.report);
-    })
-   
+  
+
   }, [page]);
   return (
     <div className="App container ">
       <h3> <FormattedMessage id="title" /></h3>
       <div className="form-group row">
-        <label for="changeLange" className="col-sm-4 "><FormattedMessage id="changeLanguage" /></label>
-        <div className="col-sm-8">
-
-          <select className="form-control " id="changeLange" onChange={(e) => { props.changeLanguage(e.target.value) }}
-          >
+        <label for="changeLange" className=" col-sm-6 ">
+          <FormattedMessage id="changeLanguage" />
+        </label>
+        <div className='col-sm-6'>
+          <select className="form-control " id="changeLange" onChange={(e) => { props.changeLanguage(e.target.value) }}>
             <option value="te" selected>తెలుగు</option>
             <option value="en">English</option>
           </select>
         </div>
       </div>
       <div className='row'>
-        {allData ? getAllData(allData) : 'Loading...'}
+        {allData ? getAllData(allData) : <Loader/>}
       </div>
-
-      <div className='row'>
-        {cases ? getCases(cases, indiaCases) : 'Loading...'}
-      </div>
+      <CaseDetails cases={cases}  setCases={setCases} ></CaseDetails>
     </div>
   );
 }
-function getCases (cases, indiaCases) {
-  return <table className='table '>
-    <thead>
 
-      <tr>
-        <th> <FormattedMessage id="country" /></th>
-        <th> <FormattedMessage id="cases" /></th>
-        <th> <FormattedMessage id="recovered" /></th>
-        <th> <FormattedMessage id="deaths" /></th>
-      </tr>
-    </thead>
-    <tbody>
-      {indiaCases ? getIndianCaseDetails(indiaCases) : <tr><td>'Loading...'</td></tr>}
-      {cases.map(caseItem => getCaseDetails(caseItem))}
-    </tbody>
-  </table>
-}
 
-function getCaseDetails (caseItem) {
-  return (<tr key={caseItem.Country}>
-    <td><FormattedMessage id={caseItem.Country} /></td>
-    <td>{caseItem.TotalCases}</td>
-    <td>{caseItem.TotalRecovered}</td>
-    <td>{caseItem.TotalDeaths}</td>
-
-  </tr >)
-}
-
-function getIndianCaseDetails (caseItem) {
-  return (<tr key={caseItem.Country}>
-    <td><FormattedMessage id="India" /></td>
-    <td>{caseItem.cases}</td>
-    <td>{caseItem.recovered}</td>
-    <td>{caseItem.deaths}</td>
-
-  </tr >)
-}
 function getAllData (allData) {
   return <table className="table total-details">
 
@@ -100,7 +66,7 @@ function getAllData (allData) {
         <td>{allData.deaths}</td>
 
       </tr>
-      
+
     </tbody>
   </table>
 }
---
+++ src/CaseDetails.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { FormattedMessage } from 'react-intl';
+import Loader from './Loader';
+import './CaseDetails.css'
+export default function CaseDetails (props) {
+  const { cases } = props;
+  const [searchCountry, setSearchCountry] = useState('');
+  const [indianCases, setIndianCases] = useState(null);
+
+  const [localCopyCases, setLocalCopyCases] = useState(null);
+  const [page] = useState(1);
+
+  useEffect(() => {
+    setLocalCopyCases(cases);
+    if (cases) {
+      let filterCases = cases.filter(caseItem =>
+        caseItem.Country === 'India');
+      setIndianCases(filterCases);
+    }
+
+  }, [cases]);
+
+  return (<>
+    <div className="form-group row">
+      <label for="changeLange " className='col-sm-6 ' >
+        <FormattedMessage id="FilterByCountry" />
+      </label>
+      <div className='col-sm-6'>
+        <input className="form-control" value={searchCountry} onChange={(e) => {
+          setSearchCountry(e.target.value);
+          let toLowerCaseValue = e.target.value.toLowerCase();
+          let filterCases = cases.filter(caseItem =>
+            caseItem.Country.toLowerCase().includes(toLowerCaseValue));
+          setLocalCopyCases(filterCases);
+        }}></input>
+      </div>
+    </div>
+    <div className='row'>
+      {localCopyCases ? getCases(localCopyCases, indianCases) :<Loader></Loader>}
+    </div>
+  </>)
+
+}
+function getCases (cases, indiaCases) {
+  return (
+    <div className='col-sm-12 total-details-wrapper'>
+      <table className='table '>
+        <thead>
+          <tr>
+            <th> <FormattedMessage id="country" /></th>
+            <th> <FormattedMessage id="NewCases" /></th>
+            <th> <FormattedMessage id="cases" /></th>
+            <th> <FormattedMessage id="recovered" /></th>
+            <th> <FormattedMessage id="deaths" /></th>
+          </tr>
+        </thead>
+        <tbody>
+          {indiaCases.map(caseItem => getCaseDetails(caseItem))}
+          {cases.map(caseItem => getCaseDetails(caseItem))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function getCaseDetails (caseItem) {
+  return (<tr key={caseItem.Country}>
+    <td><FormattedMessage id={caseItem.Country} /></td>
+    <td>{caseItem.TotalCases}</td>
+    <td>{caseItem.NewCases}</td>
+    <td>{caseItem.TotalRecovered}</td>
+    <td>{caseItem.TotalDeaths}</td>
+
+  </tr >)
+}
+
+function getIndianCaseDetails (caseItem) {
+  return (<tr key={caseItem.Country}>
+    <td><FormattedMessage id="India" /></td>
+    <td>{caseItem.cases}</td>
+    <td>{caseItem.recovered}</td>
+    <td>{caseItem.deaths}</td>
+
+  </tr >)
+}
